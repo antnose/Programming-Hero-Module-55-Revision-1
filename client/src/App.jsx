e9deb03f8316dec7fb85f4c3eaf873e0978bc7ed
antnose <antnose28@gmail.com>
2025-03-09 21:08:47 +0600

    Complete 1st video of module 55 recap-1st

---
+++ client/src/App.jsx
@@ -0,0 +1,73 @@
+import { useEffect, useState } from "react";
+import "./App.css";
+import { data } from "react-router-dom";
+
+function App() {
+  const [users, setUsers] = useState([]);
+  useEffect(() => {
+    fetch("http://localhost:3001/users")
+      .then((res) => res.json())
+      .then((data) => setUsers(data));
+  }, []);
+
+  const handleCreateUser = (e) => {
+    e.preventDefault();
+    const form = e.target;
+    const name = form.name.value;
+    const email = form.email.value;
+    const user = { name, email };
+    // console.log(name, email);
+
+    fetch("http://localhost:3001/users", {
+      method: "POST",
+      headers: {
+        "content-type": "application/json",
+      },
+      body: JSON.stringify(user),
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+        form.reset();
+      });
+
+    // useEffect(() => {
+    //   fetch("http://localhost:3001/users", {
+    //     method: "POST",
+    //     headers: {
+    //       "content-type": "application/json",
+    //     },
+    //     body: JSON.stringify(user),
+    //   })
+    //     .then((res) => res.json())
+    //     .then((data) => {
+    //       console.log(data);
+    //       form.reset();
+    //     });
+    // }, []);
+  };
+
+  return (
+    <>
+      <h1>Users</h1>
+      <form onSubmit={handleCreateUser}>
+        <input type="text" name="name" id="" />
+        <br />
+        <br />
+        <input type="email" name="email" id="" />
+        <br />
+        <br />
+        <input type="submit" value="Add User" id="" />
+      </form>
+      <div>
+        {users.map((user) => (
+          <p key={user.id}>
+            {user.id} : {user.name} : {user.email}
+          </p>
+        ))}
+      </div>
+    </>
+  );
+}
+
+export default App;
---
+++ server/index.js
@@ -0,0 +1,87 @@
+const { MongoClient, ServerApiVersion } = require("mongodb");
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const port = process.env.PORT || 3001;
+
+// Here all middleware
+app.use(express.json());
+app.use(cors());
+
+// Dummy Data
+const users = [
+  {
+    id: 1,
+    name: "Alice Johnson",
+    email: "alice.johnson@example.com",
+  },
+  {
+    id: 2,
+    name: "Bob Smith",
+    email: "bob.smith@example.com",
+  },
+  {
+    id: 3,
+    name: "Charlie Davis",
+    email: "charlie.davis@example.com",
+  },
+  {
+    id: 4,
+    name: "Dana Lee",
+    email: "dana.lee@example.com",
+  },
+  {
+    id: 5,
+    name: "Eve Williams",
+    email: "eve.williams@example.com",
+  },
+];
+
+// MongoDB Code Start
+
+const uri =
+  "mongodb+srv://admin:admin@cluster0.to58y.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await client.close();
+  }
+}
+run().catch(console.dir);
+
+// MongoDB Code End
+
+app.get("/", (req, res) => {
+  res.send("Hello World");
+});
+
+app.get("/users", (req, res) => {
+  res.send(users);
+});
+
+app.post("/users", (req, res) => {
+  console.log("Api is hitting ");
+  console.log(req.body);
+});
+
+app.listen(port, () => {
+  console.log(`Server started on ${port}`);
+});
